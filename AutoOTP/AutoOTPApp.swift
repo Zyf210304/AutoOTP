fed7cb34f136e9b34a1e330c34bdc474953ef6bd
张亚飞 <672233580@qq.com>
2022-04-19 09:55:10 +0800

    Initial Commit

---
+++ AutoOTP/AutoOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOTPApp.swift
+//  AutoOTP
+//
+//  Created by 张亚飞 on 2022/4/19.
+//
+
+import SwiftUI
+
+@main
+struct AutoOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
